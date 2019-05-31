5d8376ae79cad34fa3a6c841558b4d802e376ef3
Ed Santos <esantos@live.ca>
2019-05-30 20:00:04 -0400

    Initial Commit

---
+++ NavigationView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationView
+//
+//  Created by Ed Santos on 2019-05-30.
+//  Copyright © 2019 Ed Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
