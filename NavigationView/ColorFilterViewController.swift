b88e8be48f7170605da78baa8e7c0c064a7df33a
Ed Santos <esantos@live.ca>
2019-06-03 19:03:20 -0400

    first commit with nonzero exit code error

---
+++ NavigationView/ColorFilterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorFilterViewController.swift
+//  NavigationView
+//
+//  Created by Ed Santos on 2019-05-31.
+//  Copyright © 2019 Ed Santos. All rights reserved.
+//
+
+import UIKit
+
+class ColorFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
